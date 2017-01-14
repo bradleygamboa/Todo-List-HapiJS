e802caba0e64e8e754a02702b4b73643dbf72060
Bradley Gamboa <bradleygamboa@Bradleys-MacBook-Air.local>
2017-01-14 02:07:06 -0600

    handlebars and GET working

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 'use strict';
 
 const Hapi = require('hapi');
-
+const Path = require('path');
+const Hoek = require('hoek');
 const server = new Hapi.Server();
 server.connection({ port: process.env.PORT || 3000, host: 'localhost' });
+
+server.register(require('vision'), (err) => {
+
+    Hoek.assert(!err, err);
+
+server.views({  
+    engines: {
+        html: require('handlebars')
+    },
+    path: 'views',
+    layoutPath: 'views/layout',
+    layout: 'default',
+    // helpersPath: 'views/helpers',
+    //partialsPath: 'views/partials'
+});
+});
 
 var express = require('express');
 var app = express();
 var Sequelize = require('sequelize');
-//enables handlebars
-var exphbs = require('express-handlebars');
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
+
 //allows text to be read from form
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
+
 //enable PUSH to be overriden into DELETE
 var methodOverride = require('method-override');
 app.use(methodOverride('_method'));
@@ -41,39 +56,34 @@ server.route({
     method: 'GET',
     path: '/',
     handler: function (request, reply) {
-        reply('Hello, world!');
+        models.TodoItem.findAll({}).then(function(data) {
+        reply.view('index', { tasks: data });
+    })
     }
 });
-// app.get('/', function(req, res) {
-//     //Grabs all todos and displays on main page
-//     models.TodoItem.findAll({}).then(function(data) {
-//         res.render('home', { tasks: data });
-//     })
+
+// app.post('/todos/', function(req, res) {
+//     //Adds a new todo
+//     models.TodoItem.create({
+//         task: req.body.newTask,
+//         done: false
+//     }).then(function() {
+//         res.redirect('/');
+//     });
 
 // });
 
-app.post('/todos/', function(req, res) {
-    //Adds a new todo
-    models.TodoItem.create({
-        task: req.body.newTask,
-        done: false
-    }).then(function() {
-        res.redirect('/');
-    });
+// app.delete('/todos/:id', function(req, res) {
+//     //Deletes todo with specific ids
+//     models.TodoItem.destroy({
+//         where: {
+//             id: req.params.id
+//         }
+//     }).then(function(data) {
+//         res.redirect('/');
+//     })
 
-});
-
-app.delete('/todos/:id', function(req, res) {
-    //Deletes todo with specific ids
-    models.TodoItem.destroy({
-        where: {
-            id: req.params.id
-        }
-    }).then(function(data) {
-        res.redirect('/');
-    })
-
-});
+// });
 
 server.start((err) => {
 
